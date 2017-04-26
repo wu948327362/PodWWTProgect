12df76ce5b9c347bf68fa09289c8307fe85a25bc
wuwentao <wuwentao@qiyi.com>
2017-04-26 22:42:31 +0800

    初始化提交

--- PodWWTProgect/PodWWTProgect/AppDelegate.m
+++ PodWWTProgect/PodWWTProgect/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"这是初始化的测试");
     return YES;
 }
 
